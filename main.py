77e1962858727a5ce19c80f207f4790b1464faa2
chavellnscott <140358825+chavellnscott@users.noreply.github.com>
2024-01-16 02:38:56 -0600

    Update main.py
    generates header

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 
+#generate header
+import pandas as pd
+from pathlib import Path
+csv_path="Resources/budget_data.csv"
+df = pd.read_csv (csv_path)
+#csvreader=csv.reader(path)
+#print(next(csv.reader))
